f9b813461308071ee5127b737f005b9babd673eb
Juslin Jukka <jusju@haaga-helia.fi>
2018-01-31 15:19:43 +0200

    first commit

---
+++ src/main/java/com/example/jukankirjakauppa/JukankirjakauppaApplication.java
@@ -0,0 +1,12 @@
+package com.example.jukankirjakauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukankirjakauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukankirjakauppaApplication.class, args);
+	}
+}
